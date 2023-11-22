19dbb2598921aeae82eabd992d6b1719fdbb1674
Alicetair Cyric Toyoca <alestaircyrilculanag.coyoca@cit.edu>
2023-11-22 23:31:49 +0800

    bls

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,54 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 import { Button } from "@material-ui/core";
-import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min.js";
-import axios from "axios"; // Import axios
 import logo from "../assets/images/logo-white.png";
 import { NAV_HOVER_STYLE } from "../assets/styles/styles.js";
-import storeSample from "../assets/images/storesample.png";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min.js";
 
 const NavigationBar = () => {
   const [isHovered, setIsHovered] = useState(false);
-  const [userData, setUserData] = useState(null);
+
   const loc = useLocation();
-  const history = useHistory();
-
-  // Function to fetch user data
-  const fetchUserData = () => {
-    axios
-      .get("http://localhost:8080/api/getAllUsers")
-      .then((response) => {
-        setUserData(response.data);
-      })
-      .catch((error) => {
-        alert("Error fetching data:", error);
-      });
-  };
-
-  // useEffect hook to fetch user data when the component mounts and not on the landing page
-  useEffect(() => {
-    if (loc.pathname !== "/landing") {
-      fetchUserData();
-    }
-  }, [loc.pathname]);
-
-  const handleLogout = () => {
-    // Add logic for logout (e.g., clear authentication token, reset user state, etc.)
-    // After logout, redirect to the landing page
-    setUserData(null); // Assuming clearing user data
-    history.push("/landing");
-  };
+  console.log(loc);
 
   return (
     <nav className="flex justify-between w-full py-3 px-16 bg-primary">
       <img src={logo} alt="Palit logo" className="w-30 h-16" />
       <div className="flex items-center justify-center gap-8">
         <ul className="flex gap-8 items-center justify-center text-white text-xl">
-          {userData === null && (
-            <li className={NAV_HOVER_STYLE}>
-              <Link to="/">Home</Link>
-            </li>
-          )}
+          <li className={NAV_HOVER_STYLE}>
+            <Link to="/">Home</Link>
+          </li>
           <li className={NAV_HOVER_STYLE}>
             <Link to="/aboutus">About us</Link>
           </li>
@@ -60,52 +32,26 @@ const NavigationBar = () => {
           </li>
         </ul>
 
-        <div className="flex items-center">
-          {userData ? (
-            <>
-              <Button
-                variant="outlined"
-                style={{
-                  textTransform: "none",
-                  width: "120px",
-                  fontSize: "20px",
-                  color: isHovered ? "#F4D23E" : "white",
-                  fontFamily: "Poppins",
-                  transition: "color 0.3s, border-color 0.3s",
-                  border:"none"
-                }}
-                onClick={handleLogout}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                Log Out
-              </Button>
-
-              <img src={storeSample} alt="User" className=" ml-7 w-14 h-14 rounded-full" />
-            </>
-          ) : (
-            <Link to="/signin" style={{ textDecoration: "none" }}>
-              <Button
-                variant="outlined"
-                style={{
-                  textTransform: "none",
-                  borderColor: "white",
-                  width: "120px",
-                  fontSize: "18px",
-                  color: isHovered ? "#F4D23E" : "white",
-                  borderColor: isHovered ? "#F4D23E" : "white",
-                  fontFamily: "Poppins",
-                  borderRadius: "20px",
-                  transition: "color 0.3s, border-color 0.3s",
-                }}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                Sign In
-              </Button>
-            </Link>
-          )}
-        </div>
+        <Link to="/signin" style={{ textDecoration: "none" }}>
+          <Button
+            variant="outlined"
+            style={{
+              textTransform: "none",
+              borderColor: "white",
+              width: "120px",
+              fontSize: "18px",
+              color: isHovered ? "#F4D23E" : "white",
+              borderColor: isHovered ? "#F4D23E" : "white",
+              fontFamily: "Poppins",
+              borderRadius: "20px",
+              transition: "color 0.3s, border-color 0.3s",
+            }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            Sign In
+          </Button>
+        </Link>
       </div>
     </nav>
   );

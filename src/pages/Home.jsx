e7d1c4c5c6f491420a96bf06c278a00af9700755
Mark Kenneth S. Badilla <iammkb2002@gmail.com>
2023-12-01 17:06:27 +0800

    aslkdjflaskd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,29 +40,29 @@ const mapOptions = {
 
 const R = 6371;
 const deg2rad = (deg) => deg * (Math.PI / 180);
+
 const getDistance = (lat1, lon1, lat2, lon2) => {
+  console.log("gettingDistance");
   const dLat = deg2rad(lat2 - lat1);
   const dLon = deg2rad(lon2 - lon1);
   const a =
-      Math.sin(dLat / 2) ** 2 +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) ** 2;
+    Math.sin(dLat / 2) ** 2 +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) ** 2;
   return R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)) * 1000;
 };
 
 // Define a function to update the location of a given locationId
 const updateLocation = (locationId, location) => {
   axios
-      .put(`/api/updateLocationById/${locationId}`, location)
-      .then((response) => {
-        console.log("Location updated successfully");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    .put(`http://localhost:8080/api/updateLocationById/${locationId}`, location)
+    .then((response) => {
+      console.log("Location updated successfully");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
-
-
-
+///////////start
 function Home() {
   const { user, updateLocation } = useContext(UserContext);
   const [currentPosition, setCurrentPosition] = useState(null);
@@ -71,83 +71,78 @@ function Home() {
   const mapRef = useRef();
 
   // Get the user location from the user context
-  const userLocation = user.location;
+  let userLocation = user.account.location;
 
-    ///////////////////////////////////////////////////////
-
-
-// Define a state variable to store the locations
+  // Define a state variable to store the locations
   const [locations, setLocations] = useState([]);
 
-// Define a state variable to store the error
+  // Define a state variable to store the error
   const [error, setError] = useState(null);
 
-// Fetch all locations from the API when the component mounts
+  // Fetch all locations from the API when the component mounts
   useEffect(() => {
     axios
-        .get("/api/getAllLocations")
-        .then((response) => {
-          setLocations(response.data);
-        })
-        .catch((error) => {
-          setError(error);
+      .get("http://localhost:8080/api/getAllUsers")
+      .then((response) => {
+        console.log("Fetch all locations from the API when the component mounts");
+        setLocations(prevState => response.data);
+        console.log("locations: ", locations);
+        response.data.forEach((location) => {
+          console.log("entering for loop");
+
+            console.log("locationsdafasf: ", location);
+            const distance = getDistance(
+              userLocation.latitude,
+              userLocation.longitude,
+              location.latitude,
+              location.longitude
+            );
+            console.log("distanceasdfad: ", distance);
+            if (distance > 200) {
+              // Calculate the new coordinates that are 200m away from the user location
+              // Use a simple formula that assumes the Earth is a sphere
+              // This is not very accurate but good enough for demonstration purposes
+              const bearing = Math.atan2(
+                location.longitude - userLocation.longitude,
+                location.latitude - userLocation.latitude
+              ); // Angle between the user location and the location in radians
+              const newLatitude =
+                userLocation.latitude +
+                (200 / 6371e3) * Math.cos(bearing) * (180 / Math.PI); // Convert radians to degrees
+              const newLongitude =
+                userLocation.longitude +
+                (200 / 6371e3) * Math.sin(bearing) * (180 / Math.PI);
+              // Create a new location object with the updated coordinates
+              const newLocation = {
+                ...location,
+                latitude: newLatitude,
+                longitude: newLongitude,
+              };
+              // Update the location using the API
+              updateLocation(location.locationId, newLocation);
+            
+          }
         });
-  }, []);
-
-// Loop through the locations and update each one if it is not within 200m of the user location
-  useEffect(() => {
-    locations.forEach((location) => {
-      const distance = getDistance(
-          userLocation.latitude,
-          userLocation.longitude,
-          location.latitude,
-          location.longitude
-      );
-      if (distance > 200) {
-        // Calculate the new coordinates that are 200m away from the user location
-        // Use a simple formula that assumes the Earth is a sphere
-        // This is not very accurate but good enough for demonstration purposes
-        const bearing = Math.atan2(
-            location.longitude - userLocation.longitude,
-            location.latitude - userLocation.latitude
-        ); // Angle between the user location and the location in radians
-        const newLatitude =
-            userLocation.latitude +
-            (200 / 6371e3) * Math.cos(bearing) * (180 / Math.PI); // Convert radians to degrees
-        const newLongitude =
-            userLocation.longitude +
-            (200 / 6371e3) * Math.sin(bearing) * (180 / Math.PI);
-        // Create a new location object with the updated coordinates
-        const newLocation = {
-          ...location,
-          latitude: newLatitude,
-          longitude: newLongitude,
-        };
-        // Update the location using the API
-        updateLocation(location.locationId, newLocation);
-      }
-    });
-  }, [locations, userLocation]);
-
-
-  ///////////////////////////////////////////////////////
-
-  console.log("/map, user: ", user);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, [userLocation]);
 
   const onMapLoad = useCallback(
-      (map) => {
-        mapRef.current = map;
-        if (currentPosition) {
-          mapRef.current.setCenter(currentPosition);
-        }
-      },
-      [currentPosition]
+    (map) => {
+      mapRef.current = map;
+      if (currentPosition) {
+        mapRef.current.setCenter(currentPosition);
+      }
+    },
+    [currentPosition]
   );
 
   const renderVendorMarkerIcon = () => {
     if (
-        typeof window.google === "object" &&
-        typeof window.google.maps === "object"
+      typeof window.google === "object" &&
+      typeof window.google.maps === "object"
     ) {
       return {
         url: marker,
@@ -168,9 +163,9 @@ function Home() {
     };
 
     const watchId = navigator.geolocation.watchPosition(
-        updateLocationInContext,
-        (error) => console.log(error),
-        { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+      updateLocationInContext,
+      (error) => console.log(error),
+      { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
     );
 
     return () => navigator.geolocation.clearWatch(watchId);
@@ -209,102 +204,107 @@ function Home() {
 
   useEffect(() => {
     axios
-        .get("http://localhost:8080/api/getAllUsers")
-        .then(({ data }) => {
-          // Filter out users who are vendors and within 200 meters of the current user
-          const vendorsNearby = data.filter((otherUser) => {
-            console.log("otherUser: ", otherUser);
-            if (
-                !user.account.isVendor &&
-                otherUser.account.isVendor &&
-                getDistance(
-                    currentPosition.lat,
-                    currentPosition.lng,
-                    otherUser.location.latitude,
-                    otherUser.location.longitude
-                ) <= 200
-            ) {
-              return true;
-            }
-            return false;
+      .get("http://localhost:8080/api/getAllUsers")
+      .then(({ data }) => {
+        // Filter out users who are vendors and within 200 meters of the current user
+        const vendorsNearby = data.filter((user) => {
+          console.log("otherUser: ", user);
+          if (
+            !user.account.isVendor &&
+            user.account.isVendor &&
+            getDistance(
+              currentPosition.lat,
+              console.log(
+                "current post lat lng ",
+                currentPosition.lat,
+                currentPosition.lng
+              ),
+              currentPosition.lng,
+              user.location.latitude,
+              user.location.longitude
+            ) <= 200
+          ) {
+            return true;
+          }
+          return false;
+        });
+
+        // Mark the vendors on the map with markers
+        vendorsNearby.forEach((vendor) => {
+          const vendorMarker = new window.google.maps.Marker({
+            position: {
+              lat: vendor.location.latitude,
+              lng: vendor.location.longitude,
+            },
+            map: mapRef.current, // Assuming you have a map reference
+            icon: renderVendorMarkerIcon(), // Define the icon for vendors
           });
 
-          // Mark the vendors on the map with markers
-          vendorsNearby.forEach((vendor) => {
-            const vendorMarker = new window.google.maps.Marker({
-              position: {
-                lat: vendor.location.latitude,
-                lng: vendor.location.longitude,
-              },
-              map: mapRef.current, // Assuming you have a map reference
-              icon: renderVendorMarkerIcon(), // Define the icon for vendors
-            });
-
-            // You can add click event handling for the markers if needed
-            vendorMarker.addListener("click", () => {
-              window.location.href = `/store/${vendor.accountId}`;
-            });
+          // You can add click event handling for the markers if needed
+          vendorMarker.addListener("click", () => {
+            window.location.href = `/store/${vendor.accountId}`;
           });
-        })
-        .catch((error) => console.error("Error fetching users: ", error));
+        });
+      })
+      .catch((error) => console.error("Error fetching users: ", error));
   }, [currentPosition, user.account.isVendor]);
 
   return (
-      <>
-        <div className="w-full font-custom">
-          <NavigationBar />
-          <LoadScript googleMapsApiKey="AIzaSyBNM-CYx7dA0gckgBmybtovang7Bvp8lK0">
-            <div>
-              <GoogleMap
-                  mapContainerStyle={mapContainerStyle}
-                  zoom={17}
-                  className="flex-1"
-                  options={mapOptions}
-                  onLoad={onMapLoad}
-              >
-                {currentPosition && (
-                    <Marker
-                        position={currentPosition}
-                        icon={renderVendorMarkerIcon()}
-                    />
-                )}
-                {currentPosition && (
-                    <Circle
-                        center={currentPosition}
-                        radius={200}
-                        options={{
-                          strokeColor: "#0071B3",
-                          strokeOpacity: 0.8,
-                          strokeWeight: 2,
-                          fillColor: "#0071B3",
-                          fillOpacity: 0.05,
-                        }}
-                    />
-                )}
-                <MapSlidingBox
-                    showSlider={showSlider}
-                    handleSliderToggle={handleSliderToggle}
+    <>
+      <div className="w-full font-custom">
+        <NavigationBar />
+        <LoadScript googleMapsApiKey="AIzaSyBNM-CYx7dA0gckgBmybtovang7Bvp8lK0">
+          <div>
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              zoom={17}
+              className="flex-1"
+              options={mapOptions}
+              onLoad={onMapLoad}
+            >
+              {currentPosition && (
+                <Marker
+                  position={currentPosition}
+                  icon={renderVendorMarkerIcon()}
                 />
-              </GoogleMap>
-              <button
-                  style={{
-                    backgroundColor: "white",
-                    position: "absolute",
-                    left: "30px",
-                    bottom: "30px",
-                    padding: "10px",
-                    borderRadius: "5px",
-                    boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
-                    transition: "left 0.3s ease, bottom 0.3s ease",
+              )}
+              {currentPosition && (
+                <Circle
+                  center={currentPosition}
+                  radius={200}
+                  options={{
+                    strokeColor: "#0071B3",
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: "#0071B3",
+                    fillOpacity: 0.05,
                   }}
-                  onClick={handleReport}
-              >
-                <MdOutlineReportGmailerrorred size={30} />
-              </button>
-            </div>
-          </LoadScript>
-        </div>
-      </>
+                />
+              )}
+              <MapSlidingBox
+                showSlider={showSlider}
+                handleSliderToggle={handleSliderToggle}
+              />
+            </GoogleMap>
+            <button
+              style={{
+                backgroundColor: "white",
+                position: "absolute",
+                left: "30px",
+                bottom: "30px",
+                padding: "10px",
+                borderRadius: "5px",
+                boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
+                transition: "left 0.3s ease, bottom 0.3s ease",
+              }}
+              onClick={handleReport}
+            >
+              <MdOutlineReportGmailerrorred size={30} />
+            </button>
+          </div>
+        </LoadScript>
+      </div>
+    </>
   );
 }
 

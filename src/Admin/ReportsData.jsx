ae7d2b44989eae9a2d80a9cccbe7f0f04ad31ee3
keisamae <kheisa.selma@cit.edu>
2023-12-06 20:46:36 +0800

    dashboard changes

--- src/Admin/ReportsData.jsx
+++ src/Admin/ReportsData.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Paper, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@material-ui/core";
+import {
+  Paper,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from "@material-ui/core";
 import CustomButton from "../components/CustomButton";
 
 const ReportsData = () => {
@@ -10,19 +17,18 @@ const ReportsData = () => {
   const [open, setOpen] = useState(false);
   const [openDelete, setOpenDelete] = useState(false);
   const [deleteIndex, setDeleteIndex] = useState(-1);
-  // Add new states for the search and filter inputs
   const [searchSender, setSearchSender] = useState("");
   const [searchMessage, setSearchMessage] = useState("");
   const [filterResolved, setFilterResolved] = useState("All");
 
-
   useEffect(() => {
     const fetchReportData = async () => {
       try {
-        const response = await axios.get("http://localhost:8080/api/getAllReports");
+        const response = await axios.get(
+          "http://localhost:8080/api/getAllReports"
+        );
         setReportData(response.data);
         setEditedReportData(response.data);
-        console.log(response.data);
       } catch (error) {
         console.error("Error fetching reports data:", error);
       }
@@ -54,7 +60,6 @@ const ReportsData = () => {
     setEditedReportData(newEditedReportData);
   };
 
-  // Add a new function to handle the checkbox change
   const handleCheckboxChange = (e, index) => {
     const { checked } = e.target;
     const newEditedReportData = [...editedReportData];
@@ -65,20 +70,20 @@ const ReportsData = () => {
   const handleSaveClick = async () => {
     for (let report of editedReportData) {
       try {
-        const response = await axios.put(
+        await axios.put(
           `http://localhost:8080/api/updateReportById/${report.reportId}`,
           report
         );
-        console.log(response.data);
       } catch (error) {
         console.error("Error updating report data:", error);
       }
     }
     setEdit(0);
     try {
-      const response = await axios.get("http://localhost:8080/api/getAllReports");
+      const response = await axios.get(
+        "http://localhost:8080/api/getAllReports"
+      );
       setReportData(response.data);
-      console.log(response.data);
     } catch (error) {
       console.error("Error fetching reports data:", error);
     }
@@ -87,43 +92,37 @@ const ReportsData = () => {
   const handleOpenDeleteDialog = () => {
     setOpenDelete(true);
   };
-  
+
   const handleCloseDeleteDialog = () => {
     setOpenDelete(false);
-  };  
+  };
 
   const handleDeleteClick = async (index) => {
     setDeleteIndex(index);
     handleOpenDeleteDialog();
   };
-  
 
   const handleDeleteReport = async () => {
-    // Get the report id from the editedReportData array using the deleteIndex
     const reportId = editedReportData[deleteIndex].reportId;
-    // Call the deleteReportById endpoint using axios.delete and pass the reportId as a parameter
     try {
-      const response = await axios.delete(
+      await axios.delete(
         `http://localhost:8080/api/deleteReportById/${reportId}`
       );
-      console.log(response.data);
     } catch (error) {
       console.error("Error deleting report data:", error);
     }
-    // Fetch the updated report data from the getAllReports endpoint and update the state
     try {
-      const response = await axios.get("http://localhost:8080/api/getAllReports");
+      const response = await axios.get(
+        "http://localhost:8080/api/getAllReports"
+      );
       setReportData(response.data);
       setEditedReportData(response.data);
     } catch (error) {
       console.error("Error fetching reports data:", error);
     }
-    // Close the delete dialog
     handleCloseDeleteDialog();
   };
-  
 
-  // Add a new function to handle the search input change
   const handleSearchChange = (e) => {
     const { name, value } = e.target;
     if (name === "searchSender") {
@@ -133,32 +132,33 @@ const ReportsData = () => {
     }
   };
 
-  // Add a new function to handle the filter input change
   const handleFilterChange = (e) => {
     const { value } = e.target;
     setFilterResolved(value);
   };
 
-  // Add a new function to filter the report data based on the search and filter inputs
   const filterReportData = () => {
     let filteredReportData = reportData;
-    // Filter by sender name
     if (searchSender) {
       filteredReportData = filteredReportData.filter((report) =>
         report.senderId.email.toLowerCase().includes(searchSender.toLowerCase())
       );
     }
-    // Filter by message content
     if (searchMessage) {
       filteredReportData = filteredReportData.filter((report) =>
-        report.messageContent.toLowerCase().includes(searchMessage.toLowerCase())
+        report.messageContent
+          .toLowerCase()
+          .includes(searchMessage.toLowerCase())
       );
     }
-    // Filter by resolved status
     if (filterResolved === "Resolved") {
-      filteredReportData = filteredReportData.filter((report) => report.isResolved);
+      filteredReportData = filteredReportData.filter(
+        (report) => report.isResolved
+      );
     } else if (filterResolved === "Pending") {
-      filteredReportData = filteredReportData.filter((report) => !report.isResolved);
+      filteredReportData = filteredReportData.filter(
+        (report) => !report.isResolved
+      );
     }
     return filteredReportData;
   };
@@ -166,60 +166,72 @@ const ReportsData = () => {
   return (
     <Paper elevation={3} className="p-5 border rounded-3xl font-custom">
       <div>
-        <h1 className="text-2xl font-bold pb-6">Report</h1>
-        <CustomButton
-          label={edit === 1 ? "Save" : "Edit"}
-          onClick={edit === 1 ? handleOpenDialog : handleEditClick}
-        ></CustomButton>
-        {/* Add new inputs for the search and filter features */}
-        <div className="flex justify-between items-center py-4">
-          <div className="flex items-center">
-            <label htmlFor="searchSender" className="pr-2">Search by sender:</label>
-            <input
-              type="text"
-              id="searchSender"
-              name="searchSender"
-              value={searchSender}
-              onChange={handleSearchChange}
-            />
-          </div>
-          <div className="flex items-center">
-            <label htmlFor="searchMessage" className="pr-2">Search by message:</label>
-            <input
-              type="text"
-              id="searchMessage"
-              name="searchMessage"
-              value={searchMessage}
-              onChange={handleSearchChange}
-            />
-          </div>
-          <div className="flex items-center">
-            <label htmlFor="filterResolved" className="pr-2">Filter by resolved:</label>
-            <select
-              id="filterResolved"
-              name="filterResolved"
-              value={filterResolved}
-              onChange={handleFilterChange}
-            >
-              <option value="All">All</option>
-              <option value="Resolved">Resolved</option>
-              <option value="Pending">Pending</option>
-            </select>
-          </div>
+        <div className="flex">
+          <h1 className="text-2xl font-bold pb-6 text-[#0071B3]">Report</h1>
+          <CustomButton
+            label={edit === 1 ? "Save" : "Edit"}
+            onClick={edit === 1 ? handleOpenDialog : handleEditClick}
+            btnStyle={
+              edit === 1
+                ? "text-blue-500 ml-5 text-sm font-thin"
+                : "text-red-500 ml-5 text-sm font-thin"
+            }
+          ></CustomButton>
         </div>
         <table className="w-full">
           <thead className="text-left border-b border-[#0071B3] text-slate-500">
             <tr>
-              <th className="w-1/5 pb-2">Sender</th>
-              <th className="w-1/5 pb-2">Message Content</th>
+              <th className="w-1/5 pb-2">
+                <div className="flex items-center">
+                  Sender
+                  <input
+                    type="text"
+                    id="searchSender"
+                    name="searchSender"
+                    value={searchSender}
+                    onChange={handleSearchChange}
+                    placeholder="Search sender"
+                    className="ml-5 border-b border-[#0071B3] text-slate-500 text-sm font-thin"
+                  />
+                </div>
+              </th>
+              <th className="w-1/5 pb-2">
+                <div className="flex items-center">
+                  Message Content
+                  <input
+                    type="text"
+                    id="searchMessage"
+                    name="searchMessage"
+                    value={searchMessage}
+                    onChange={handleSearchChange}
+                    placeholder="Search message"
+                    className="ml-5 border-b border-[#0071B3] text-slate-500 text-sm font-thin"
+                  />
+                </div>
+              </th>
               <th className="w-1/5 pb-2">Time Stamp</th>
-              <th className="w-1/10 pb-2">isResolved</th>
-              {/* Use a conditional rendering to display the Delete column header only when edit is 1 */}
+              <th className="w-1/10 pb-2">
+                <div className="flex items-center">
+                  <label htmlFor="filterResolved" className="pr-2">
+                    Resolved
+                  </label>
+                  <select
+                    id="filterResolved"
+                    name="filterResolved"
+                    value={filterResolved}
+                    onChange={handleFilterChange}
+                    className="ml-5 border-b border-[#0071B3] text-slate-500 text-sm font-thin"
+                  >
+                    <option value="All">All</option>
+                    <option value="Resolved">Resolved</option>
+                    <option value="Pending">Pending</option>
+                  </select>
+                </div>
+              </th>
               {edit === 1 && <th className="w-1/10 pb-2">Delete</th>}
             </tr>
           </thead>
           <tbody>
-            {/* Use the filterReportData function to display the filtered data */}
             {filterReportData().map((report, index) => (
               <tr key={report.reportId}>
                 <td className="py-2">
@@ -279,60 +291,54 @@ const ReportsData = () => {
                     <CustomButton
                       label="Delete"
                       onClick={() => handleDeleteClick(index)}
-                      ></CustomButton>
-                    </td>
-                  )}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <Dialog
-          open={openDelete}
-          onClose={handleCloseDeleteDialog}
-        >
-          <DialogTitle>Confirm deletion</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Are you sure you want to delete this report?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <CustomButton
-              label="Cancel"
-              onClick={handleCloseDeleteDialog}
-            ></CustomButton>
-            <CustomButton
-              label="Confirm Deletion"
-              onClick={handleDeleteReport}
-            ></CustomButton>
-          </DialogActions>
-        </Dialog>
-        <Dialog
-          open={open && edit === 1}
-          onClose={handleCloseDialog}
-        >
-          <DialogTitle>Confirm changes</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Are you sure you want to make any changes to the report data?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <CustomButton
-              label="Cancel"
-              onClick={handleCloseDialog}
-            ></CustomButton>
-            <CustomButton
-              label="Confirm Changes"
-              onClick={handleConfirmDialog}
-            ></CustomButton>
-          </DialogActions>
-        </Dialog>
+                    ></CustomButton>
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Dialog open={openDelete} onClose={handleCloseDeleteDialog}>
+        <DialogTitle>Confirm deletion</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this report?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions className="mr-5 mb-5">
+          <CustomButton
+            label="Cancel"
+            onClick={handleCloseDeleteDialog}
+          ></CustomButton>
+          <CustomButton
+            label="Delete"
+            onClick={handleDeleteReport}
+            btnStyle="bg-red-500 rounded-3xl text-white p-2 text-lg font-semibolds cursor-pointer p-3"
+          ></CustomButton>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={open && edit === 1} onClose={handleCloseDialog}>
+        <DialogTitle>Confirm changes</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to make any changes to the report data?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions className=" mr-5 mb-5">
+          <CustomButton
+            label="Cancel"
+            onClick={handleCloseDialog}
+          ></CustomButton>
+          <CustomButton
+            label="Save Changes"
+            onClick={handleConfirmDialog}
+            btnStyle="bg-blue-500 rounded-3xl text-white p-2 text-lg font-semibolds cursor-pointer p-3"
+          ></CustomButton>
+        </DialogActions>
+      </Dialog>
+    </Paper>
+  );
+};
 
-
-      </Paper>
-    );
-  };
-  
-  export default ReportsData;
+export default ReportsData;

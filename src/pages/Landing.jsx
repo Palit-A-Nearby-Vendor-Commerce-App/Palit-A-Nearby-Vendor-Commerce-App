5cc704433b1e4150f069e3856492afc6b259b39c
Joshua Briones <joshuabriones2003@gmail.com>
2023-12-08 15:49:01 +0800

    Landing page responsive

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -31,15 +31,15 @@ const Landing = () => {
   }, [user]);
 
   return (
-    <div className="w-full h-screen bg-primary flex flex-col bg-stroke-bg bg-center bg-no-repeat bg-cover font-custom">
+    <div className="w-full h-screen bg-primary flex flex-col bg-stroke-bg bg-center bg-no-repeat bg-cover font-custom xl:h-screen">
       <NavigationBar />
-      <div className="landing-content flex items-center gap-16 h-full text-left m-auto text-white px-20">
+      <div className="landing-content flex flex-row items-center gap-16 h-full text-left m-auto text-white xl:gap-16 lg:gap-12 lg:px-20 md:text-left md:flex-row md:gap-6 md:px-14 sm:gap-3 sm:flex-col sm:pt-10 xs:px-16 xs:text-center xs:self-center">
         <div className="flex-1">
-          <h1 className="landing-header text-7xl font-bold">
+          <h1 className="landing-header text-7xl font-bold xl:text-7xl md:text-6xl xs:text-5xl">
             <span className="text-customYellow">Palit:</span> Buy and sell on
             the go!
           </h1>
-          <p className="landing-parag mt-7 text-slate-300 mb-14">
+          <p className="landing-parag mt-7 text-slate-300 mb-14 xl:mb-14 sm:mb-5 sm:text-base xs:text-sm">
             A commerce app for moving vendors and customers. Find, chat, and buy
             from nearby sellers on the map. Convenient, fair, and secure.
             Support local livelihoods.
@@ -63,8 +63,12 @@ const Landing = () => {
             </Button>
           </Link>
         </div>
-        <div className="landing-img w-full h-fit flex flex-1">
-          <img src={hero} className="h-[640px] self-center" />
+        <div className="landing-img w-full h-fit flex flex-1 xl:flex-1 sm:flex-1 justify-center sm:flex xs:hidden">
+          <img
+            alt="bg hero"
+            src={hero}
+            className="h-[640px] self-center xl:h-[640px] lg:h-[570px] md:h-[480px] sm:h-[320px]"
+          />
         </div>
       </div>
     </div>

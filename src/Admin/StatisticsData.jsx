1d86667f52582dc6b58a2513508d0fb976779ea4
Alicetair Cyric Toyoca <alestaircyrilculanag.coyoca@cit.edu>
2023-12-10 15:53:31 +0800

    Update StatisticsData.jsx

--- src/Admin/StatisticsData.jsx
+++ src/Admin/StatisticsData.jsx
@@ -7,21 +7,21 @@ const StatisticsData = () => {
   const [totalUsers, setTotalUsers] = useState(0);
   const [totalTransactions, setTotalTransactions] = useState(0);
   const [resolutionRate, setResolutionRate] = useState(0);
-  const [reportsData, setReportsData] = useState([]); // New state variable for reports data
+  const [reportsData, setReportsData] = useState([]);
+  const [ageData, setAgeData] = useState([]);
+  
 
-
-
-  const websiteVisitorsData = {
-    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+  const ageDistributionData = {
+    labels: ['10-20', '20-30', '30-40', '40-50', '50+'],
     datasets: [
       {
-        label: "Website Visitors",
-        backgroundColor: "rgba(75,192,192,0.4)",
-        borderColor: "rgba(75,192,192,1)",
+        label: 'Age Distribution',
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
         borderWidth: 1,
-        hoverBackgroundColor: "rgba(75,192,192,0.6)",
-        hoverBorderColor: "rgba(75,192,192,1)",
-        data: [65, 59, 80, 81, 56],
+        hoverBackgroundColor: 'rgba(75,192,192,0.6)',
+        hoverBorderColor: 'rgba(75,192,192,1)',
+        data: ageData,
       },
     ],
   };
@@ -48,67 +48,91 @@ const StatisticsData = () => {
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-        data: reportsData, // Use the reports data here
+        data: reportsData,
       },
     ],
   };
 
   useEffect(() => {
-    axios
-      .get("http://localhost:8080/api/getAllUsers")
-      .then((response) => {
-        setTotalUsers(response.data.length);
-      })
-      .catch((error) => {
-        console.error(
-          "There was an error fetching the total number of users!",
-          error
-        );
-      });
-
-    // New API call to get all transactions
-    axios
-      .get("http://localhost:8080/api/getAllTransactions")
-      .then((response) => {
-        setTotalTransactions(response.data.length);
-      })
-      .catch((error) => {
-        console.error(
-          "There was an error fetching the total number of transactions!",
-          error
-        );
-      });
-
-    // New API call to get all reports and calculate resolution rate
-    axios
-      .get("http://localhost:8080/api/getAllReports")
-      .then((response) => {
-        const reports = response.data;
-        const resolvedReports = reports.filter(
-          (report) => report.isResolved === true
-        );
-        const resolutionRate =
-          (resolvedReports.length / reports.length) * 100 || 0;
-        setResolutionRate(resolutionRate);
-
-        // Calculate the number of reports received in the past week
-        const pastWeekReports = Array(7).fill(0);
-        const oneWeekAgo = moment().subtract(7, 'days');
-        reports.forEach(report => {
-          const reportDate = moment(report.timestamp);
-          if (reportDate.isAfter(oneWeekAgo)) {
-            const dayOfWeek = reportDate.day();
-            pastWeekReports[dayOfWeek]++;
-          }
+    const fetchData = () => {
+      axios
+        .get("http://localhost:8080/api/getAllUsers")
+        .then((response) => {
+          const users = response.data;
+          setTotalUsers(users.length);
+  
+          const ageCounts = {};
+          users.forEach(user => {
+            const birthDate = moment(user.birthDate);
+            const age = moment().diff(birthDate, 'years');
+  
+            let ageRange;
+            if (age < 20) ageRange = '10-20';
+            else if (age < 30) ageRange = '20-30';
+            else if (age < 40) ageRange = '30-40';
+            else if (age < 50) ageRange = '40-50';
+            else ageRange = '50+';
+  
+            ageCounts[ageRange] = (ageCounts[ageRange] || 0) + 1;
+          });
+  
+          const ageRanges = ['10-20', '20-30', '30-40', '40-50', '50+'];
+          const counts = ageRanges.map(range => ageCounts[range] || 0);
+          setAgeData(counts);
+        })
+        .catch((error) => {
+          console.error(
+            "There was an error fetching the total number of users!",
+            error
+          );
         });
-        setReportsData(pastWeekReports);
-      })
-      .catch((error) => {
-        console.error(
-          "There was an error fetching the total number of reports!",
-          error
-        );
-      });
+  
+      axios
+        .get("http://localhost:8080/api/getAllTransactions")
+        .then((response) => {
+          setTotalTransactions(response.data.length);
+        })
+        .catch((error) => {
+          console.error(
+            "There was an error fetching the total number of transactions!",
+            error
+          );
+        });
+  
+      axios
+        .get("http://localhost:8080/api/getAllReports")
+        .then((response) => {
+          const reports = response.data;
+          const resolvedReports = reports.filter(
+            (report) => report.isResolved === true
+          );
+          const resolutionRate =
+            (resolvedReports.length / reports.length) * 100 || 0;
+          setResolutionRate(resolutionRate);
+  
+          const pastWeekReports = Array(7).fill(0);
+          const oneWeekAgo = moment().subtract(7, 'days');
+          reports.forEach(report => {
+            const reportDate = moment(report.timestamp);
+            if (reportDate.isAfter(oneWeekAgo)) {
+              const dayOfWeek = reportDate.day();
+              pastWeekReports[dayOfWeek]++;
+            }
+          });
+          setReportsData(pastWeekReports);
+        })
+        .catch((error) => {
+          console.error(
+            "There was an error fetching the total number of reports!",
+            error
+          );
+        });
+    };
+  
+    fetchData();
+    const intervalId = setInterval(fetchData, 100); 
+  
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
@@ -122,7 +146,6 @@ const StatisticsData = () => {
           </div>
         </div>
 
-        {/* Total Transactions */}
         <div className="w-full md:w-1/3 px-2 mb-4">
           <div className="border border-gray-300 p-4 bg-yellow-300 rounded-lg shadow-md">
             <h2 className="text-2xl mb-2">Total Transactions</h2>
@@ -133,7 +156,6 @@ const StatisticsData = () => {
           </div>
         </div>
 
-        {/* Report Resolution Rate */}
         <div className="w-full md:w-1/3 px-2 mb-4">
           <div className="border border-gray-300 p-4 bg-red-400 rounded-lg shadow-md">
             <h2 className="text-2xl mb-2">Report Resolution Rate</h2>
@@ -145,9 +167,7 @@ const StatisticsData = () => {
         </div>
       </div>
 
-      {/* Display graphs side by side */}
       <div className="flex flex-wrap -mx-2">
-        {/* Reports Analytics Line Graph */}
         <div className="w-full md:w-1/2 px-2 mb-4 ">
           <div className="border border-gray-300 p-4 rounded shadow-md bg-white">
             <h2 className="text-2xl font-bold mb-2">
@@ -175,11 +195,31 @@ const StatisticsData = () => {
           </div>
         </div>
 
-        {/* Website Visitors Bar Graph */}
         <div className="w-full md:w-1/2 px-2 mb-4">
           <div className="border border-gray-300 p-4 rounded shadow-md bg-white">
-            <h2 className="text-2xl font-bold mb-2">Website Visitors</h2>
-            <Bar data={websiteVisitorsData} />
+            <h2 className="text-2xl font-bold mb-2">Age Distribution</h2>
+            <Bar
+              data={ageDistributionData}
+              options={{
+                scales: {
+                  xAxes: [{
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Age Ranges'
+                    }
+                  }],
+                  yAxes: [{
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Number of Users'
+                    },
+                    ticks: {
+                      stepSize: 1
+                    }
+                  }]
+                }
+              }}
+            />
           </div>
         </div>
       </div>
